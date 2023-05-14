ade4b9bad37d1328777f99c049826af1b7118d5b
Qasimali2004 <u2022414@giki.edu.pk>
2023-05-14 17:56:09 +0500

    Update classes.h
    class header file updated

--- classes.h
+++ classes.h
@@ -4,142 +4,211 @@
 
 #ifndef LIBRARY_MANAGEMENT_SYSTEM_CLASSES_H
 #define LIBRARY_MANAGEMENT_SYSTEM_CLASSES_H
+
+
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
-using namespace std;
+#include <sstream>
+#include <algorithm>
 
-class Book {
+class User
+{
 private:
-    string title;
-    string author;
-    int yearPublished;
-    bool available;
+    std::string username;
+    std::string password;
+    std::string role;
+
 public:
-    Book(string title, string author, int yearPublished) {
-        this->title = title;
-        this->author = author;
-        this->yearPublished = yearPublished;
-        this->available = true;
-    }
+    User(const std::string &u, const std::string &p, const std::string &r)
+        : username(u), password(p), role(r) {}
 
-    string getTitle() {
-        return title;
-    }
-
-    string getAuthor() {
-        return author;
-    }
-
-    int getYearPublished() {
-        return yearPublished;
-    }
-
-    bool isAvailable() {
-        return available;
-    }
-
-    void borrow() {
-        if(available) {
-            available = false;
-            cout << "Book \"" << title << "\" borrowed." << endl;
-        } else {
-            cout << "Book \"" << title << "\" is not available." << endl;
-        }
-    }
-
-    void returnBook() {
-        available = true;
-        cout << "Book \"" << title << "\" returned." << endl;
-    }
+    const std::string &getUsername() const { return username; }
+    const std::string &getPassword() const { return password; }
+    const std::string &getRole() const { return role; }
 };
 
-class User {
+class Book
+{
 private:
-    string name;
-    vector<Book*> borrowedBooks;
+    std::string title;
+    std::string author;
+    bool isAvailable;
+
 public:
-    User(string name) {
-        this->name = name;
-    }
+    Book(const std::string &t, const std::string &a)
+        : title(t), author(a), isAvailable(true) {}
 
-    string getName() {
-        return name;
-    }
-
-    vector<Book*> getBorrowedBooks() {
-        return borrowedBooks;
-    }
-
-    void borrowBook(Book* book) {
-        borrowedBooks.push_back(book);
-    }
-
-    void returnBook(Book* book) {
-        for(int i = 0; i < borrowedBooks.size(); i++) {
-            if(borrowedBooks[i] == book) {
-                borrowedBooks.erase(borrowedBooks.begin() + i);
-                book->returnBook();
-                return;
-            }
-        }
-    }
+    const std::string &getTitle() const { return title; }
+    const std::string &getAuthor() const { return author; }
+    bool getAvailability() const { return isAvailable; }
+    void setAvailability(bool available) { isAvailable = available; }
 };
 
-class Library {
+class Library
+{
 private:
-    vector<Book*> books;
-    vector<User*> users;
-public:
-    void addBook(Book* book) {
-        books.push_back(book);
-    }
+    std::vector<User *> users;
+    std::vector<Book *> books;
 
-    void addUser(User* user) {
+public:
+    void addUser(User *user)
+    {
         users.push_back(user);
     }
 
-    void borrowBook(Book* book, User* user) {
-        if(book->isAvailable()) {
-            book->borrow();
-            user->borrowBook(book);
-        } else {
-            cout << "Book \"" << book->getTitle() << "\" is not available." << endl;
+    User *getUser(const std::string &username)
+    {
+        for (User *user : users)
+        {
+            if (user->getUsername() == username)
+            {
+                return user;
+            }
+        }
+        return nullptr;
+    }
+
+    void addBook(Book *book)
+    {
+        books.push_back(book);
+    }
+
+    Book *getBook(const std::string &title)
+    {
+        for (Book *book : books)
+        {
+            if (book->getTitle() == title)
+            {
+                return book;
+            }
+        }
+        return nullptr;
+    }
+
+    void borrowBook(User *user, Book *book)
+    {
+        if (user != nullptr && book != nullptr && book->getAvailability())
+        {
+            std::cout << user->getUsername() << " has borrowed the book: " << book->getTitle() << std::endl;
+            book->setAvailability(false);
         }
     }
 
-    void returnBook(Book* book, User* user) {
-        for(int i = 0; i < users.size(); i++) {
-            if(users[i] == user) {
-                user->returnBook(book);
-                return;
+    void returnBook(User *user, Book *book)
+    {
+        if (user != nullptr && book != nullptr && !book->getAvailability())
+        {
+            std::cout << user->getUsername() << " has returned the book: " << book->getTitle() << std::endl;
+            book->setAvailability(true);
+        }
+    }
+
+    void displayBookInfo(const std::string &title)
+    {
+        Book *book = getBook(title);
+        if (book != nullptr)
+        {
+            std::cout << "Title: " << book->getTitle() << std::endl;
+            std::cout << "Author: " << book->getAuthor() << std::endl;
+            std::cout << "Availability: " << (book->getAvailability() ? "Available" : "Borrowed") << std::endl;
+        }
+        else
+        {
+            std::cout << "Book not found." << std::endl;
+        }
+    }
+
+    void displayAllBooks()
+    {
+        if (books.empty())
+        {
+            std::cout << "No books available in the library." << std::endl;
+        }
+        else
+        {
+            std::cout << "Library Books: " << std::endl;
+            for (Book *book : books)
+            {
+                std::cout << "Title: " << book->getTitle() << ", Author: " << book->getAuthor() << ", Availability: " << book->getAvailability();
             }
         }
     }
 
-    void printBooks() {
-        cout << "Library books:" << endl;
-        for(int i = 0; i < books.size(); i++) {
-            cout << books[i]->getTitle() << " by " << books[i]->getAuthor() << " (" << books[i]->getYearPublished() << ")";
-            if(books[i]->isAvailable()) {
-                cout << " [available]" << endl;
-            } else {
-                cout << " [not available]" << endl;
+    void saveLibraryData(const Library &library, const std::string &filename)
+    {
+        std::ofstream file(filename);
+        if (file.is_open())
+        {
+            // Save users
+            for (const User *user : library.users)
+            {
+                file << "User," << user->getUsername() << "," << user->getPassword() << "," << user->getRole() << std::endl;
             }
+
+            // Save books
+            for (const Book *book : library.books)
+            {
+                file << "Book," << book->getTitle() << "," << book->getAuthor() << "," << book->getAvailability() << std::endl;
+            }
+
+            file.close();
+            std::cout << "Library data saved successfully." << std::endl;
+        }
+        else
+        {
+            std::cerr << "Unable to save library data to file: " << filename << std::endl;
         }
     }
 
-    void printUsers() {
-        cout << "Library users:" << endl;
-        for(int i = 0; i < users.size(); i++) {
-            cout << users[i]->getName() << endl;
-            vector<Book*> borrowedBooks = users[i]->getBorrowedBooks();
-            if(borrowedBooks.size() > 0) {
-                cout << "Borrowed books:" << endl;
-                for(int j = 0; j < borrowedBooks.size(); j++){
-                    cout << borrowedBooks[j]->getTitle() << " by " << borrowedBooks[j]->getAuthor() << " (" << borrowedBooks[j]->getYearPublished() << ")" << endl;
+    void loadLibraryData(Library &library, const std::string &filename)
+    {
+        std::ifstream file(filename);
+        if (file.is_open())
+        {
+            library.users.clear();
+            library.books.clear();
+
+            std::string line;
+            while (std::getline(file, line))
+            {
+                std::stringstream ss(line);
+                std::string type;
+                std::getline(ss, type, ',');
+
+                if (type == "User")
+                {
+                    std::string username, password, role;
+                    std::getline(ss, username, ',');
+                    std::getline(ss, password, ',');
+                    std::getline(ss, role, ',');
+                    library.addUser(new User(username, password, role));
+                }
+                else if (type == "Book")
+                {
+                    std::string title, author;
+                    bool availability;
+                    std::getline(ss, title, ',');
+                    std::getline(ss, author, ',');
+                    ss >> availability;
+                    library.addBook(new Book(title, author));
+                    library.getBook(title)->setAvailability(availability);
                 }
             }
+
+            file.close();
+            std::cout << "Library data loaded successfully." << std::endl;
+        }
+        else
+        {
+            std::cerr << "Unable to load library data from file: " << filename << std::endl;
         }
     }
+
+    void removeBook(Book *book)
+    {
+        books.erase(std::remove(books.begin(), books.end(), book), books.end());
+    }
 };
-#endif //LIBRARY_MANAGEMENT_SYSTEM_CLASSES_H
+#endif
